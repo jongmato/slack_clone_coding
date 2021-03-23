d2cffba675bfb033e6b23f62a724bffd80f64b86
jonghyun <jonghyeon2046@gmail.com>
2021-03-23 17:23:12 +0900

    [UPDATE]Router 수정

--- front-jh/layouts/App/index.tsx
+++ front-jh/layouts/App/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import loadable from '@loadable/component';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
+
 const LogIn = loadable(() => import('@pages/LogIn'));
 const SignUp = loadable(() => import('@pages/SignUp'));
-const Channel = loadable(() => import('@pages/Channel'));
-const DirectMessage = loadable(() => import('@pages/DirectMessage'));
+const Workspace = loadable(() => import('@layouts/Workspace'));
+
 
 const App = () => {
     return (
@@ -13,8 +14,7 @@ const App = () => {
             <Redirect exact path="/" to="/login" />
             <Route path="/login" component={LogIn} />
             <Route path="/signup" component={SignUp} />
-            <Route path="/workspace/channel" component={Channel} />
-            <Route path="/workspace/dm" component={DirectMessage} />
+            <Route path="/workspace" component={Workspace} />
         </Switch>
     );
 };
