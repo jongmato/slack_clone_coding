897c371a7de20c4519594c4d3977c4ab55337cfc
jonghyun <jonghyeon2046@gmail.com>
2021-03-23 16:45:27 +0900

    [UPDATE]App.tsx 파일 App 폴더 안에 index.tsx로 수정

---
+++ front-jh/layouts/App/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import loadable from '@loadable/component';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+const LogIn = loadable(() => import('@pages/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp'));
+const Channel = loadable(() => import('@pages/Channel'));
+const DirectMessage = loadable(() => import('@pages/DirectMessage'));
+
+const App = () => {
+    return (
+        <Switch>
+            <Redirect exact path="/" to="/login" />
+            <Route path="/login" component={LogIn} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/workspace/channel" component={Channel} />
+            <Route path="/workspace/dm" component={DirectMessage} />
+        </Switch>
+    );
+};
+
+export default App;
