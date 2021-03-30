0ae981e07e223e5cc72747692cd573eac17250bd
jonghyun <jonghyeon2046@gmail.com>
2021-03-30 16:39:01 +0900

    [UPDATE]useInput hook 함수 타입 수정

--- front-jh/hooks/useInput.ts
+++ front-jh/hooks/useInput.ts
@@ -1,13 +1,13 @@
-import { Dispatch, SetStateAction, useCallback, useState } from 'react';
+import { ChangeEvent, Dispatch, SetStateAction, useCallback, useState } from 'react';
 
-type Handler = (e: any) => void;
-type ReturnTypes<T = any> = [T, Handler, Dispatch<SetStateAction<T>>];
-const useInput = <T = any>(initialValue: T): ReturnTypes<T> => {
-  const [value, setValue] = useState(initialValue);
-  const handler = useCallback((e) => {
-    setValue(e.target.value);
-  }, []);
-  return [value, handler, setValue];
+type ReturnTypes<T> = [T, (e: ChangeEvent<HTMLInputElement>) => void, Dispatch<SetStateAction<T>>];
+
+const useInput = <T extends string | number>(initialData: T): ReturnTypes<T> => {
+    const [value, setValue] = useState(initialData);
+    const handler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        setValue((e.target.value as unknown) as T);
+    }, []);
+    return [value, handler, setValue];
 };
 
 export default useInput;
