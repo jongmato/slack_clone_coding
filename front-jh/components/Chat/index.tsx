02937673ca1712e4ea9ef325a9ec41264f04e6ad
jonghyun <jonghyeon2046@gmail.com>
2021-04-06 16:55:15 +0900

    [UPDATE]이미지 업로드 기능 추가

--- front-jh/components/Chat/index.tsx
+++ front-jh/components/Chat/index.tsx
@@ -1,35 +1,41 @@
 import { IChat, IDM } from '@typings/db';
-import React, { VFC, memo, useMemo } from 'react';
+import React, { FC, memo, useMemo } from 'react';
 import { ChatWrapper } from '@components/Chat/styles';
 import gravatar from 'gravatar';
 import dayjs from 'dayjs';
 import regexifyString from 'regexify-string';
 import { Link, useParams } from 'react-router-dom';
+
 interface Props {
     data: IDM | IChat;
 }
 
-const Chat: VFC<Props> = ({ data }) => {
+const BACK_URL = 'http://localhost:3095';
+const Chat: FC<Props> = memo(({ data }) => {
     const { workspace } = useParams<{ workspace: string; channel: string }>();
     const user = 'Sender' in data ? data.Sender : data.User;
 
-    const result = useMemo(
+    const result = useMemo<(string | JSX.Element)[] | JSX.Element>(
         () =>
-            regexifyString({
-                input: data.content,
-                pattern: /@\[(.+?)]\((\d+?)\)|\n/g,
-                decorator(match, index) {
-                    const arr: string[] | null = match.match(/@\[(.+?)]\((\d+?)\)/)!;
-                    if (arr) {
-                        return (
-                            <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
-                                @{arr[1]}
-                            </Link>
-                        );
-                    }
-                    return <br key={index} />;
-                },
-            }),
+            data.content.startsWith('uploads\\') ? (
+                <img src={`${BACK_URL}/${data.content}`} style={{ maxHeight: 200 }} />
+            ) : (
+                regexifyString({
+                    pattern: /@\[(.+?)]\((\d+?)\)|\n/g,
+                    decorator(match, index) {
+                        const arr: string[] | null = match.match(/@\[(.+?)]\((\d+?)\)/)!;
+                        if (arr) {
+                            return (
+                                <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
+                                    @{arr[1]}
+                                </Link>
+                            );
+                        }
+                        return <br key={index} />;
+                    },
+                    input: data.content,
+                })
+            ),
         [data.content],
     );
     return (
@@ -46,6 +52,6 @@ const Chat: VFC<Props> = ({ data }) => {
             </div>
         </ChatWrapper>
     );
-};
+});
 
 export default memo(Chat);
