6bd5d5c92009b5d3c9ec87c9af38a772adad4439
jonghyun <jonghyeon2046@gmail.com>
2021-04-06 18:23:29 +0900

    [UPDATE]이미지 드래그앤드롭 기능 추가

--- front-jh/pages/Channel/index.tsx
+++ front-jh/pages/Channel/index.tsx
@@ -1,11 +1,204 @@
-import Workspace from '@layouts/Workspace/index';
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { Container, Header, DragOver } from '@pages/Channel/styles';
+import ChatList from '@components/ChatList';
+import ChatBox from '@components/ChatBox';
+import useInput from '@hooks/useInput';
+import makeSection from '@utils/makeSection';
+import axios from 'axios';
+import { useParams } from 'react-router';
+import useSWR, { useSWRInfinite } from 'swr';
+import fetcher from '@utils/fetcher';
+import useSocket from '@hooks/useSocket';
+import { Scrollbars } from 'react-custom-scrollbars';
+import { IChannel, IChat, IUser } from '@typings/db';
+import InviteChannelModal from '@components/InviteChannelModal';
+import { toast, ToastContainer } from 'react-toastify';
 
 const Channel = () => {
+    const { workspace, channel } = useParams<{ workspace: string; channel: string }>();
+    const { data: myData } = useSWR(`/api/users`, fetcher);
+    const [chat, onChangeChat, setChat] = useInput('');
+    const { data: channelData } = useSWR<IChannel>(`/api/workspaces/${workspace}/channels/${channel}`, fetcher);
+    const { data: chatData, mutate: mutateChat, revalidate, setSize } = useSWRInfinite<IChat[]>(
+        (index) => `/api/workspaces/${workspace}/channels/${channel}/chats?perPage=20&page=${index + 1}`,
+        fetcher,
+        {
+            onSuccess(data) {
+                if (data?.length === 1) {
+                    setTimeout(() => {
+                        scrollbarRef.current?.scrollToBottom();
+                    }, 100);
+                }
+            },
+        },
+    );
+    const { data: channelMembersData } = useSWR<IUser[]>(
+        myData ? `/api/workspaces/${workspace}/channels/${channel}/members` : null,
+        fetcher,
+    );
+    const [socket] = useSocket(workspace);
+    const isEmpty = chatData?.[0]?.length === 0;
+    const isReachingEnd = isEmpty || (chatData && chatData[chatData.length - 1]?.length < 20) || false;
+    const scrollbarRef = useRef<Scrollbars>(null);
+    const [showInviteChannelModal, setShowInviteChannelModal] = useState(false);
+    const [dragOver, setDragOver] = useState(false);
+
+    const onSubmitForm = useCallback(
+        (e) => {
+            e.preventDefault();
+            if (chat?.trim() && chatData && channelData && myData) {
+                const savedChat = chat;
+                mutateChat((prevChatData) => {
+                    prevChatData?.[0].unshift({
+                        id: (chatData[0][0]?.id || 0) + 1,
+                        content: savedChat,
+                        UserId: myData.id,
+                        User: myData,
+                        ChannelId: channelData.id,
+                        Channel: channelData,
+                        createdAt: new Date(),
+                    });
+                    return prevChatData;
+                }, false).then(() => {
+                    localStorage.setItem(`${workspace}-${channel}`, new Date().getTime().toString());
+                    setChat('');
+                    scrollbarRef.current?.scrollToBottom();
+                });
+                axios
+                    .post(`/api/workspaces/${workspace}/channels/${channel}/chats`, {
+                        content: savedChat,
+                    })
+                    .then(() => {
+                        revalidate();
+                    })
+                    .catch(console.error);
+            }
+        },
+        [chat, chatData, myData, channelData, workspace, channel],
+    );
+
+    const onMessage = useCallback(
+        (data: IChat) => {
+            // id = 상대방 id
+            if ((data.Channel.name === channel && data.content.startsWith('uploads\\')) || data.UserId !== myData?.id) {
+                mutateChat((chatData) => {
+                    chatData?.[0].unshift(data);
+                    return chatData;
+                }, false).then(() => {
+                    if (scrollbarRef.current) {
+                        if (
+                            scrollbarRef.current.getScrollHeight() <
+                            scrollbarRef.current.getClientHeight() + scrollbarRef.current.getScrollTop() + 150
+                        ) {
+                            console.log('scrollToBottom', scrollbarRef.current?.getValues());
+                            setTimeout(() => {
+                                scrollbarRef.current?.scrollToBottom();
+                            }, 100);
+                        } else {
+                            toast.success('새 메시지가 도착했습니다.', {
+                                onClick() {
+                                    scrollbarRef.current?.scrollToBottom();
+                                },
+                                closeOnClick: true,
+                            });
+                        }
+                    }
+                });
+            }
+        },
+        [channel, myData, mutateChat],
+    );
+
+    useEffect(() => {
+        socket?.on('message', onMessage);
+        return () => {
+            socket?.off('message', onMessage);
+        };
+    }, [socket, onMessage]);
+
+    //로딩 시 스크롤바 아래에 위치
+    useEffect(() => {
+        localStorage.setItem(`${workspace}-${channel}`, new Date().getTime().toString());
+    }, [workspace, channel]);
+
+    const onDrop = useCallback(
+        (e) => {
+            e.preventDefault();
+            const formData = new FormData();
+            if (e.dataTransfer.items) {
+                // Use DataTransferItemList interface to access the file(s)
+                for (let i = 0; i < e.dataTransfer.items.length; i++) {
+                    // If dropped items aren't files, reject them
+                    if (e.dataTransfer.items[i].kind === 'file') {
+                        const file = e.dataTransfer.items[i].getAsFile();
+                        console.log('... file[' + i + '].name = ' + file.name);
+                        formData.append('image', file);
+                    }
+                }
+            } else {
+                // Use DataTransfer interface to access the file(s)
+                for (let i = 0; i < e.dataTransfer.files.length; i++) {
+                    console.log('... file[' + i + '].name = ' + e.dataTransfer.files[i].name);
+                    formData.append('image', e.dataTransfer.files[i]);
+                }
+            }
+            axios.post(`/api/workspaces/${workspace}/channels/${channel}/images`, formData).then(() => {
+                setDragOver(false);
+                localStorage.setItem(`${workspace}-${channel}`, new Date().getTime().toString());
+            });
+        },
+        [workspace, channel],
+    );
+
+    const onDragOver = useCallback((e) => {
+        e.preventDefault();
+        setDragOver(true);
+    }, []);
+
+    const onClickInviteChannel = useCallback(() => {
+        setShowInviteChannelModal(true);
+    }, []);
+    const onCloseModal = useCallback(() => {
+        setShowInviteChannelModal(false);
+    }, []);
+
+    if (!myData || !myData) {
+        return null;
+    }
+
+    const chatSections = makeSection(chatData ? chatData.flat().reverse() : []);
     return (
-        <Workspace>
-            <div>channel</div>
-        </Workspace>
+        <Container onDrop={onDrop} onDragOver={onDragOver}>
+            <Header>
+                <span>#{channel}</span>
+                <div className="header-right">
+                    <span>{channelMembersData?.length}</span>
+                    <button
+                        onClick={onClickInviteChannel}
+                        className="c-button-unstyled p-ia__view_header__button"
+                        aria-label="Add people to #react-native"
+                        data-sk="tooltip_parent"
+                        type="button"
+                    >
+                        <i className="c-icon p-ia__view_header__button_icon c-icon--add-user" aria-hidden="true" />
+                    </button>
+                </div>
+            </Header>
+            <ChatList chatSections={chatSections} ref={scrollbarRef} setSize={setSize} isReachingEnd={isReachingEnd} />
+            <ChatBox
+                chat={chat}
+                onChangeChat={onChangeChat}
+                onSubmitForm={onSubmitForm}
+                placeholder={`Message #${channel}`}
+            />
+            <InviteChannelModal
+                show={showInviteChannelModal}
+                onCloseModal={onCloseModal}
+                setShowInviteChannelModal={setShowInviteChannelModal}
+            />
+            <ToastContainer position="bottom-center" />
+            {dragOver && <DragOver>업로드</DragOver>}
+        </Container>
     );
 };
 
--- front-jh/pages/DirectMessage/index.tsx
+++ front-jh/pages/DirectMessage/index.tsx
@@ -1,11 +1,178 @@
-import Workspace from '@layouts/Workspace/index';
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { DragOver } from '@pages/Channel/styles';
+import { Container, Header } from '@pages/DirectMessage/styles';
+import gravatar from 'gravatar';
+import useSWR, { useSWRInfinite } from 'swr';
+import { useParams } from 'react-router';
+import fetcher from '@utils/fetcher';
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+import useInput from '@hooks/useInput';
+import axios from 'axios';
+import { IDM } from '@typings/db';
+import makeSection from '@utils/makeSection';
+import { Scrollbars } from 'react-custom-scrollbars';
+import useSocket from '@hooks/useSocket';
+import { toast } from 'react-toastify';
 
 const DirectMessage = () => {
+    const { workspace, id } = useParams<{ workspace: string; id: string }>();
+    const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+    const { data: myData } = useSWR(`/api/users`, fetcher);
+    const [chat, onChangeChat, setChat] = useInput('');
+    const { data: chatData, mutate: mutateChat, revalidate, setSize } = useSWRInfinite<IDM[]>(
+        (index) => `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=${index + 1}`,
+        fetcher,
+        {
+            onSuccess(data) {
+                if (data?.length === 1) {
+                    setTimeout(() => {
+                        scrollbarRef.current?.scrollToBottom();
+                    }, 100);
+                }
+            },
+        },
+    );
+    const [socket] = useSocket(workspace);
+    const isEmpty = chatData?.[0]?.length === 0;
+    const isReachingEnd = isEmpty || (chatData && chatData[chatData.length - 1]?.length < 20) || false;
+    const scrollbarRef = useRef<Scrollbars>(null);
+    const [dragOver, setDragOver] = useState(false);
+
+    const onSubmitForm = useCallback(
+        (e) => {
+            e.preventDefault();
+            if (chat?.trim() && chatData) {
+                const savedChat = chat;
+                mutateChat((prevChatData) => {
+                    prevChatData?.[0].unshift({
+                        id: (chatData[0][0]?.id || 0) + 1,
+                        content: savedChat,
+                        SenderId: myData.id,
+                        Sender: myData,
+                        ReceiverId: userData.id,
+                        Receiver: userData,
+                        createdAt: new Date(),
+                    });
+                    return prevChatData;
+                }, false).then(() => {
+                    localStorage.setItem(`${workspace}-${id}`, new Date().getTime().toString());
+                    setChat('');
+                    scrollbarRef.current?.scrollToBottom();
+                });
+                axios
+                    .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
+                        content: chat,
+                    })
+                    .then(() => {
+                        revalidate();
+                    })
+                    .catch(console.error);
+            }
+        },
+        [chat, chatData, myData, userData, workspace, id],
+    );
+
+    const onMessage = useCallback(
+        (data: IDM) => {
+            // id = 상대방 id
+            if (data.SenderId === Number(id) && myData.id !== Number(id)) {
+                mutateChat((chatData) => {
+                    chatData?.[0].unshift(data);
+                    return chatData;
+                }, false).then(() => {
+                    if (scrollbarRef.current) {
+                        if (
+                            scrollbarRef.current.getScrollHeight() <
+                            scrollbarRef.current.getClientHeight() + scrollbarRef.current.getScrollTop() + 150
+                        ) {
+                            console.log('scrollToBottom', scrollbarRef.current?.getValues());
+                            setTimeout(() => {
+                                scrollbarRef.current?.scrollToBottom();
+                            }, 100);
+                        } else {
+                            toast.success('새 메시지가 도착했습니다.', {
+                                onClick() {
+                                    scrollbarRef.current?.scrollToBottom();
+                                },
+                                closeOnClick: true,
+                            });
+                        }
+                    }
+                });
+            }
+        },
+        [id, myData, mutateChat],
+    );
+
+    useEffect(() => {
+        socket?.on('dm', onMessage);
+        return () => {
+            socket?.off('dm', onMessage);
+        };
+    }, [socket, onMessage]);
+
+    //로딩 시 스크롤바 아래에 위치
+    useEffect(() => {
+        localStorage.setItem(`${workspace}-${id}`, new Date().getTime().toString());
+    }, [workspace, id]);
+
+    const onDrop = useCallback(
+        (e) => {
+            e.preventDefault();
+            const formData = new FormData();
+            if (e.dataTransfer.items) {
+                // Use DataTransferItemList interface to access the file(s)
+                for (let i = 0; i < e.dataTransfer.items.length; i++) {
+                    // If dropped items aren't files, reject them
+                    if (e.dataTransfer.items[i].kind === 'file') {
+                        const file = e.dataTransfer.items[i].getAsFile();
+                        console.log('... file[' + i + '].name = ' + file.name);
+                        formData.append('image', file);
+                    }
+                }
+            } else {
+                // Use DataTransfer interface to access the file(s)
+                for (let i = 0; i < e.dataTransfer.files.length; i++) {
+                    console.log('... file[' + i + '].name = ' + e.dataTransfer.files[i].name);
+                    formData.append('image', e.dataTransfer.files[i]);
+                }
+            }
+            axios.post(`/api/workspaces/${workspace}/dms/${id}/images`, formData).then(() => {
+                setDragOver(false);
+                localStorage.setItem(`${workspace}-${id}`, new Date().getTime().toString());
+                revalidate();
+            });
+        },
+        [workspace, id, revalidate],
+    );
+
+    const onDragOver = useCallback((e) => {
+        e.preventDefault();
+        setDragOver(true);
+    }, []);
+
+    if (!userData || !myData) {
+        return null;
+    }
+
+    const chatSections = makeSection(chatData ? chatData.flat().reverse() : []);
+
     return (
-        <Workspace>
-            <div>DM</div>
-        </Workspace>
+        <Container onDrop={onDrop} onDragOver={onDragOver}>
+            <Header>
+                <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+                <span>{userData.nickname}</span>
+            </Header>
+            <ChatList chatSections={chatSections} ref={scrollbarRef} setSize={setSize} isReachingEnd={isReachingEnd} />
+            <ChatBox
+                chat={chat}
+                onChangeChat={onChangeChat}
+                onSubmitForm={onSubmitForm}
+                placeholder={`Message ${userData.nickname}`}
+            />
+            {dragOver && <DragOver>업로드</DragOver>}
+        </Container>
     );
 };
 
